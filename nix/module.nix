5b23af2a2b99f2ffce738f8d5b687c34ca79497c
Zhaith Izaliel <zhaith.izaliel@ethereal-edelweiss.cloud>
2025-08-06 08:11:57 +0200

    fix(module): remove systemd service in favor for just installing the package

--- nix/module.nix
+++ nix/module.nix
@@ -3,10 +3,8 @@
   lib,
   ...
 }: let
-  inherit (lib) mkEnableOption mkOption types mkIf getExe concatStringsSep optional;
+  inherit (lib) mkEnableOption mkOption types mkIf;
   cfg = config.programs.iio-niri;
-
-  generatedArgs = concatStringsSep " " ((optional (cfg.monitor != null) "--monitor ${cfg.monitor}") ++ cfg.extraArgs);
 in {
   options.programs.iio-niri = {
     enable = mkEnableOption "IIO-Niri";
@@ -16,38 +14,11 @@ in {
       default = iio-niri;
       description = "The iio-niri package to use.";
     };
-
-    monitor = mkOption {
-      type = types.nullOr types.nonEmptyStr;
-      default = null;
-      description = "The monitor to rotate. Can be null.";
-    };
-
-    extraArgs = mkOption {
-      type = types.listOf types.nonEmptyStr;
-      default = [];
-      description = "Extra arguments to pass to IIO-Niri";
-    };
-
-    target = mkOption {
-      type = types.nonEmptyStr;
-      default = "niri.service";
-      description = "The Niri target to start IIO-Niri after.";
-    };
   };
 
   config = mkIf cfg.enable {
-    systemd.services.iio-niri = {
-      description = "IIO-Niri";
-      wantedBy = [cfg.target];
-      after = [cfg.target];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${getExe cfg.package} ${generatedArgs}";
-        Restart = "on-failure";
-      };
-    };
-
     hardware.sensor.iio.enable = true;
+
+    environment.systemPackages = [cfg.package];
   };
 }
